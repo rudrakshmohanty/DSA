ccb1259d5ba33a9911387d63139b538d5e1b4b6a
rudrakshmohanty <rudrakshmohanty05@gmail.com>
2025-02-20 18:55:41 +0530

    .

--- CPP/Arrays/Easy/largest_element.cpp
+++ CPP/Arrays/Easy/largest_element.cpp
@@ -1,11 +1,20 @@
-#include <iostream>
-
+#include<iostream>
 using namespace std;
 
 int main(){
     int n;
     cin>>n;
+    int arr[n];
     for(int i=0; i<n; i++){
-        cout<<i<<endl;
+        cin>>arr[i];
     }
+    printf("Largest element in the array is: ");
+    int max = arr[0];
+    for(int i=1; i<n; i++){
+        if(arr[i]>max){
+            max = arr[i];
+        }
+    }
+    cout<<max;
+    return 0;
 }
